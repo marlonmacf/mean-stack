95fd93ae82fb18ce19938bba4d550f48d57bddf4
Marlon Andrel <marlonmacf@gmail.com>
2017-04-28 10:37:52 -0300

    Created the todo-app Part III (not tested yet).

---
+++ Tutorial/Examples/Part III/todo-app/app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var index = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// default middlewares setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes setup
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// catch 500 to error handler (set locals, only providing error in development)
+app.use(function (err, req, res) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+// use native Node promises to connect to MongoDB (Mongoose)
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/todo-api')
+    .then(function () {
+        console.log('[mongoose] connection succesful')
+    }, null)
+    .catch(function (err) {
+        console.error('[mongoose] ' + err)
+    });
+
+module.exports = app;
